492c822d010f2d7535e61ea7ec56d92dd5a861d1
Steven Vondruska <vondruska@users.noreply.github.com>
2022-11-15 11:21:28 -0500

    data-raw isn't as portable as I thought it was

--- wrapper.sh
+++ wrapper.sh
@@ -18,7 +18,7 @@ EXIT_CODE=$?
 # Adapted from https://stackoverflow.com/a/30938702
 OUTPUT_FILTERED=$(echo "$OUTPUT" | sed -r "s/\x1B\[(([0-9]{1,2})?(;)?([0-9]{1,2})?)?[m,K,H,f,J]//g")
 
-curl -fsS -m 10 --retry 5 --data-raw "$OUTPUT_FILTERED" -o /dev/null https://hc-ping.com/"$UUID"/"$EXIT_CODE"
+curl -fsS -m 10 --retry 5 --data "$OUTPUT_FILTERED" -o /dev/null https://hc-ping.com/"$UUID"/"$EXIT_CODE"
 
 if [ $OUTPUT_LOGS -eq 1 ]; then
   echo "$OUTPUT"
